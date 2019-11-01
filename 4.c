927098ce740e40ac9121bf1b2b7d0df7da7faae5
hyemipung <poonghm@naver.com>
2019-11-01 20:08:19 +0900

    함수이

--- 4.c
+++ 4.c
@@ -1,8 +1,10 @@
 #include “myProject.h"
-int printMyName1(void)
+int Myname4(void)//함수이름
+
 {
-printf (“my name is 풍헤미\n”);
-return 1; 
+printf (“my name is 풍헤미\n”);//이름 풍혜미
+
+return 1; //리턴
 
 
 }
