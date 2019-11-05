4cb0ef5d2d6e915ef1355b86b3ff937f475576a1
hyemipung <poonghm@naver.com>
2019-11-05 21:42:40 +0900

    ph

--- 4.c
+++ 4.c
@@ -1,8 +1,8 @@
 #include “myProject.h"
 int printMyName4(void)
 {
-printf (“my name is [hye mi pung]\n”);// name p
-return 1; //return 
+printf (“my name is [hye mi pung]\n”);// name phm
+return 1; 
 
 }
 
